815df917a266403c2d384612bc4c2f00856c37bf
paulslevin <paul.slevin@cantab.net>
2016-03-16 21:56:21 +0000

    Complete project.
    There is some ugly repetition but the code passes the linting.

--- Module3/Module3.py
+++ Module3/Module3.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python
-import itertools
-import copy
-
+"""Coursera Algorithms Project 4."""
 
 def set_matrix_value(matrix, key, letter, score1, score2, score3):
+    """Helper function for build_scoring_matrix."""
     if key == "-" or letter == "-":
         matrix[key][letter] = score1
     elif key != letter:
@@ -14,17 +13,20 @@ def set_matrix_value(matrix, key, letter, score1, score2, score3):
 
 def build_scoring_matrix(alphabet, diag_score, 
                               off_diag_score, dash_score):
+    """Builds scoring matrix."""
     augmented = alphabet | {"-"}
     matrix = {letter: {} for letter in augmented}
-    for key, letter in itertools.product(matrix, augmented):
-        set_matrix_value(matrix, key, letter,
-                         dash_score, off_diag_score, diag_score)
+    for key in matrix:
+        for letter in augmented:
+            set_matrix_value(matrix, key, letter,
+                             dash_score, off_diag_score, diag_score)
     return matrix
 
 
 def compute_alignment_matrix(seq_x, seq_y, scoring_matrix, global_flag):
-    matrix = {idx + 1: {} for idx, _ in enumerate(seq_x)}
-    matrix[0] = {0: 0}
+    """Computes alignment matrix."""
+    matrix = [[0 for __ in seq_y] + [0] for _ in seq_x]
+    matrix.append([0 for __ in seq_y] + [0])
     for idx, val in enumerate(seq_x):
         value = matrix[idx][0] + scoring_matrix[val]["-"]
         if global_flag:
@@ -37,16 +39,20 @@ def compute_alignment_matrix(seq_x, seq_y, scoring_matrix, global_flag):
             matrix[0][idx + 1] = value
         else:
             matrix[0][idx + 1] = max(0, value)
-    for (id1, val1), (id2, val2) in itertools.product(enumerate(seq_x),
-                                                      enumerate(seq_y)):
-        matrix[id1 + 1][id2 + 1] = max(
-            matrix[id1][id2] + scoring_matrix[val1][val2],
-            matrix[id1][id2 + 1] + scoring_matrix[val1]["-"],
-            matrix[id1 + 1][id2] + scoring_matrix["-"][val2])
+    for id1, val1 in enumerate(seq_x):
+        for id2, val2 in enumerate(seq_y):
+            value = max(matrix[id1][id2] + scoring_matrix[val1][val2],
+                         matrix[id1][id2 + 1] + scoring_matrix[val1]["-"],
+                         matrix[id1 + 1][id2] + scoring_matrix["-"][val2])
+            if global_flag:
+                matrix[id1 + 1][id2 + 1] = value
+            else:
+                matrix[id1 + 1][id2 + 1] = max(value, 0)
     return matrix
 
         
 def compute_global_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
+    """Computes global alignment matrix."""
     idx, idy = len(seq_x), len(seq_y)
     score = alignment_matrix[idx][idy]
     seq_x_prime, seq_y_prime = "", ""
@@ -77,10 +83,13 @@ def compute_global_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
 
 
 def compute_local_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
-    start_row = max(alignment_matrix,
-                    key=lambda x: max(alignment_matrix[x].values()))
-    start_col = max(alignment_matrix[start_row],
-                  key=lambda x: alignment_matrix[start_row][x])
+    """Computes local alignment matrix."""
+    max_value = max(sum(alignment_matrix, []))
+    for idx, row in enumerate(alignment_matrix):
+        if max_value in row:
+            start_row = idx
+            start_col = row.index(max_value)
+            break
     seq_x_prime, seq_y_prime = "", ""
     score = alignment_matrix[start_row][start_col]
     idx, idy = start_row, start_col
@@ -116,94 +125,89 @@ def compute_local_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
     return score, seq_x_prime, seq_y_prime
 
     
-if __name__ == "__main__":
-    import unittest
+#if __name__ == "__main__":
+#    import unittest
 
-    class TestProjectFunctions(unittest.TestCase):
-        def test_build_scoring_matrix_simple(self):
-            outcome = build_scoring_matrix(alphabet=set(), diag_score=0,
-                                                off_diag_score=0, dash_score=0)
-            expected = {"-": {"-": 0}}
-            self.assertEqual(outcome, expected)
+#    class TestProjectFunctions(unittest.TestCase):
+#        def test_build_scoring_matrix_simple(self):
+#            outcome = build_scoring_matrix(alphabet=set(), diag_score=0,
+#                                                off_diag_score=0, dash_score=0)
+#            expected = {"-": {"-": 0}}
+#            self.assertEqual(outcome, expected)
         
-        def test_build_scoring_matrix_one_letter(self):
-            outcome = build_scoring_matrix(alphabet={"A"}, 
-                                           diag_score=2,
-                                           off_diag_score=1,
-                                           dash_score=-1)
-            expected = {"-": {"-": -1, "A": -1}, "A": {"-": -1, "A": 2}}
-            self.assertEqual(outcome, expected)
+#        def test_build_scoring_matrix_one_letter(self):
+#            outcome = build_scoring_matrix(alphabet={"A"}, 
+#                                           diag_score=2,
+#                                           off_diag_score=1,
+#                                           dash_score=-1)
+#            expected = {"-": {"-": -1, "A": -1}, "A": {"-": -1, "A": 2}}
+#            self.assertEqual(outcome, expected)
 
-        def test_build_scoring_matrix_two_letters(self):
-            outcome = build_scoring_matrix(alphabet={"A", "B"},
-                                           diag_score=2,
-                                           off_diag_score=1,
-                                           dash_score=-1)
-            expected = {"-": {"-": -1, "A": -1, "B": -1},
-                        "A": {"-": -1, "A": 2, "B": 1},
-                        "B": {"-": -1, "A": 1, "B": 2}}
-            self.assertEqual(outcome, expected)
+#        def test_build_scoring_matrix_two_letters(self):
+#            outcome = build_scoring_matrix(alphabet={"A", "B"},
+#                                           diag_score=2,
+#                                           off_diag_score=1,
+#                                           dash_score=-1)
+#            expected = {"-": {"-": -1, "A": -1, "B": -1},
+#                        "A": {"-": -1, "A": 2, "B": 1},
+#                        "B": {"-": -1, "A": 1, "B": 2}}
+#            self.assertEqual(outcome, expected)
 
-        def test_compute_alignment_matrix_simple(self):
-            outcome = compute_alignment_matrix(seq_x="", seq_y="",
-                scoring_matrix={"-": {"-": -1, "A": -1},
-                                "A": {"-": -1, "A": 2}}, global_flag=True)
-            expected = {0: {0: 0}}
-            self.assertEqual(outcome, expected)
+#        def test_compute_alignment_matrix_simple(self):
+#            outcome = compute_alignment_matrix(seq_x="", seq_y="",
+#                scoring_matrix={"-": {"-": -1, "A": -1},
+#                                "A": {"-": -1, "A": 2}}, global_flag=True)
+#            expected = [[0]]
+#            self.assertEqual(outcome, expected)
 
-        def test_compute_alignment_matrix_one_letters(self):
-            outcome = compute_alignment_matrix(seq_x="A", seq_y="-",
-                scoring_matrix={"-": {"-": -1, "A": -1},
-                                "A": {"-": -1, "A": 2}}, global_flag=True)
-            expected = {0: {0: 0, 1: -1}, 1: {0: -1, 1: -1}}
-            self.assertEqual(outcome, expected)
+#        def test_compute_alignment_matrix_one_letters(self):
+#            outcome = compute_alignment_matrix(seq_x="A", seq_y="-",
+#                scoring_matrix={"-": {"-": -1, "A": -1},
+#                                "A": {"-": -1, "A": 2}}, global_flag=True)
+#            expected = [[0, -1], [-1,  -1]]
+#            self.assertEqual(outcome, expected)
 
-        def test_compute_alignment_matrix_two_letters(self):
-            outcome = compute_alignment_matrix(seq_x="AA", seq_y="TAAT",
-                scoring_matrix={"-": {"-": -6, "A": -6, "T": -6},
-                                "A": {"-": -6, "A": 10, "T": 4},
-                                "T": {"-": -6, "A": 4, "T": 10}},
-                global_flag=True)
-            expected = {0: {0: 0, 1: -6, 2: -12, 3: -18, 4: -24},
-                        1: {0: -6, 1: 4, 2: 4, 3: -2, 4: -8},
-                        2: {0: -12, 1: -2, 2: 14, 3: 14, 4: 8}}
-            self.assertEqual(outcome, expected)
+#        def test_compute_alignment_matrix_two_letters(self):
+#            outcome = compute_alignment_matrix(seq_x="AA", seq_y="TAAT",
+#                scoring_matrix={"-": {"-": -6, "A": -6, "T": -6},
+#                                "A": {"-": -6, "A": 10, "T": 4},
+#                                "T": {"-": -6, "A": 4, "T": 10}},
+#                global_flag=True)
+#            expected = [[0, -6, -12, -18, -24],
+#                        [-6, 4, 4, -2, -8],
+#                        [-12, -2, 14, 14, 8]]
+#            self.assertEqual(outcome, expected)
             
-        def test_compute_alignment_matrix_two_letters_local(self):
-            outcome = compute_alignment_matrix(seq_x="AA", seq_y="TAAT",
-                scoring_matrix={"-": {"-": -6, "A": -6, "T": -6},
-                                "A": {"-": -6, "A": 10, "T": 4},
-                                "T": {"-": -6, "A": 4, "T": 10}},
-                global_flag=False)
-            expected = {0: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0},
-                        1: {0: 0, 1: 4, 2: 10, 3: 10, 4: 4},
-                        2: {0: 0, 1: 4, 2: 14, 3: 20, 4: 14}}
-            self.assertEqual(outcome, expected)
+#        def test_compute_alignment_matrix_two_letters_local(self):
+#            outcome = compute_alignment_matrix(seq_x="AA", seq_y="TAAT",
+#                scoring_matrix={"-": {"-": -6, "A": -6, "T": -6},
+#                                "A": {"-": -6, "A": 10, "T": 4},
+#                                "T": {"-": -6, "A": 4, "T": 10}},
+#                global_flag=False)
+#            expected = [[0, 0, 0, 0, 0], [0, 4, 10, 10, 4], [0, 4, 14, 20, 14]]
+#            self.assertEqual(outcome, expected)
             
-        def test_compute_global_alignment_two_letters(self):
-            outcome = compute_global_alignment(
-                seq_x="AA", seq_y="TAAT",
-                scoring_matrix={"-": {"-": -6, "A": -6, "T": -6},
-                                "A": {"-": -6, "A": 10, "T": 4},
-                                "T": {"-": -6, "A": 4, "T": 10}},
-                alignment_matrix={0: {0: 0, 1: -6, 2: -12, 3: -18, 4: -24},
-                                  1: {0: -6, 1: 4, 2: 4, 3: -2, 4: -8},
-                                  2: {0: -12, 1: -2, 2: 14, 3: 14, 4: 8}})
-            expected = 8, "-AA-", "TAAT"
-            self.assertEqual(outcome, expected)
+#        def test_compute_global_alignment_two_letters(self):
+#            outcome = compute_global_alignment(seq_x="AA", seq_y="TAAT",
+#                scoring_matrix={"-": {"-": -6, "A": -6, "T": -6},
+#                                "A": {"-": -6, "A": 10, "T": 4},
+#                                "T": {"-": -6, "A": 4, "T": 10}},
+#                alignment_matrix=[[0, -6, -12, -18, -24],
+#                                  [-6, 4, 4, -2, -8],
+#                                  [-12, -2, 14, 14, 8]])
+#            expected = 8, "-AA-", "TAAT"
+#            self.assertEqual(outcome, expected)
 
-        def test_local_alignment_two_letters(self):
-            outcome = compute_local_alignment(
-                seq_x="AA", seq_y="TAAT",
-                scoring_matrix={"-": {"-": -6, "A": -6, "T": -6},
-                                "A": {"-": -6, "A": 10, "T": 4},
-                                "T": {"-": -6, "A": 4, "T": 10}},
-                alignment_matrix={0: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0},
-                                  1: {0: 0, 1: 4, 2: 10, 3: 10, 4: 4},
-                                  2: {0: 0, 1: 4, 2: 14, 3: 20, 4: 14}})
-            expected = 20, "AA", "AA"
-            self.assertEqual(outcome, expected)
-
+#        def test_compute_local_alignment_two_letters(self):
+#            outcome = compute_local_alignment(seq_x="AA", seq_y="TAAT",
+#                scoring_matrix={"-": {"-": -6, "A": -6, "T": -6},
+#                                "A": {"-": -6, "A": 10, "T": 4},
+#                                "T": {"-": -6, "A": 4, "T": 10}},
+#                alignment_matrix=[[0, 0, 0, 0, 0],
+#                                  [0, 4, 10, 10, 4],
+#                                  [0, 4, 14, 20, 14]])
+#            expected = 20, "AA", "AA"
+#            self.assertEqual(outcome, expected)
                 
 
-    unittest.main()
+#    unittest.main()
